d363b9bf57539d69c6ec4466b9127ebfa9867e3e
xuzhenhui <xuzhenhui@pku.edu.cn>
2017-12-06 15:14:13 +0800

    modified

--- utils.py
+++ utils.py
@@ -5,16 +5,43 @@ def load_excel_data(path):
     data_xls = pd.read_excel(path, 'Sheet1')
     return data_xls
 
+def get_sum_grades(province):
+    if province == '海南':
+        return 900
+    elif province == '江苏':
+        return 480
+    elif province == '浙江':
+        return 810
+    elif province == '上海':
+        return 630
+    return 750
+
+def convert_grades(data):
+    for i in range(len(data['投档成绩'])):
+        data['投档成绩'][i] = data['投档成绩'][i] / get_sum_grades(data['省市'][i])
+    return data
+
 def convert_feature(train_data, test_data):
     s = {}
     train_len = len(train_data)
     test_len = len(test_data)
     del train_data['学生ID']
     del test_data['学生ID']
-    del train_data['裸眼视力(左)']
-    del test_data['裸眼视力(左)']
-    del train_data['裸眼视力(右)']
-    del test_data['裸眼视力(右)']
+    del train_data['生源省市']
+    del test_data['生源省市']
+    del train_data['科类']
+    del test_data['科类']
+    del train_data['成绩']
+    del test_data['成绩']
+    del train_data['中学']
+    del test_data['中学']
+    
+    test_data = convert_grades(test_data)
+    train_data = convert_grades(train_data)
+    # del train_data['裸眼视力(左)']
+    # del test_data['裸眼视力(左)']
+    # del train_data['裸眼视力(右)']
+    # del test_data['裸眼视力(右)']
     train_y = train_data['综合GPA']
     train_x = train_data.drop(['综合GPA'],axis=1)
     test_x = test_data
@@ -67,7 +94,16 @@ def convert_feature(train_data, test_data):
 
     return train_x, train_y, test_x
 
+def add_zero(data):
+    #for i in range(len(data)):
+    #data['专利数'].fillna(0)
+    data.fillna(value = {'专利数':0, '社会活动':0, '获奖数':0, '竞赛成绩':0})
+    return data
+
 if __name__ == '__main__':
-    data1 = load_excel_data(sys.argv[1])
-    data2 = load_excel_data(sys.argv[2])
-    convert_feature(data1,data2)
+    train_data = load_excel_data(sys.argv[1])
+    test_data = load_excel_data(sys.argv[2])
+    M = convert_feature(train_data, test_data)
+    M[0].to_csv('data/features/train_x_f_eye.csv',index=False)
+    M[1].to_csv('data/features/train_y_f_eye.csv',index=False)
+    M[2].to_csv('data/features/test_x_f_eye.csv',index=False)
--- xgboost/xgb_pro.py
+++ xgboost/xgb_pro.py
@@ -9,7 +9,11 @@ from time import clock
 import datetime
 import sys
 from sklearn import metrics
-model_name = 'xgb'
+from sklearn.cross_validation import train_test_split
+import sys
+sys.path.append('../')
+import utils as util
+#model_name = 'xgb_depth_9_preprocessed'
 n_jobs = 2
 def xgb_train(dtrain, dtest, param, offline=True, verbose=True, num_boost_round=1000):
     if verbose:
@@ -40,7 +44,7 @@ def xgb_predict(model, dtest):
     pred_y = model.predict(dtest, ntree_limit=model.best_ntree_limit)
     return pred_y
 
-def tune_xgb(dtrain, dtest):
+def tune_xgb(dtrain, dtest, model_name, depth):
     tune_reuslt_file = "./log/tune_" + model_name
     f_w = open(tune_reuslt_file, 'wb')
     def objective(args):
@@ -54,7 +58,7 @@ def tune_xgb(dtrain, dtest):
             'eta': args['learning_rate'],
             # 'gamma': args['gamma'],
             'colsample_bytree': args['colsample_bytree'],
-            'max_depth': 6, #args['max_depth'],
+            'max_depth': depth, #args['max_depth'],
             'subsample': args['subsample']
         }
         #if fs verbose = False
@@ -75,7 +79,7 @@ def tune_xgb(dtrain, dtest):
     #pdb.set_trace()
     # Searching space
     space = {
-        'n_estimators': hp.quniform("n_estimators", 100, 200, 20),
+        'n_estimators': hp.quniform("n_estimators", 100, 200, 10),
         # 'reg_lambda': hp.loguniform("reg_lambda", np.log(1), np.log(1500)),
         # 'gamma': hp.loguniform("gamma", np.log(0.1), np.log(100)),
         'learning_rate': hp.uniform("learning_rate", 0.05, 0.15),
@@ -84,20 +88,20 @@ def tune_xgb(dtrain, dtest):
         'colsample_bytree': hp.uniform("colsample_bytree", 0.5, 0.9),
     }
     #best_sln = fmin(objective, space, algo=tpe.suggest, max_evals=300)
-    best_sln = fmin(objective, space, algo=hyperopt.anneal.suggest, max_evals=300)
+    best_sln = fmin(objective, space, algo=hyperopt.anneal.suggest, max_evals=256)
     pickle.dump(best_sln,f_w,True)
     best_rmse = objective(best_sln)
     xgb_log.write(str(best_rmse) + '\n')
     f_w.close()
 
-def test(dtrain, dtest,best_n_estimators):
+def test(dtrain, dtest,best_n_estimators,model_name):
     final_result = "./log/xgb_online_result.csv"
     f_w = open(final_result, 'w')
     model = xgb_train(dtrain, dtest, init_params, offline, verbose=False,num_boost_round=best_n_estimators)
     pred_y = xgb_predict(model, dtest)
     res = pd.read_csv('../data/submission_sample.csv')
     res['综合GPA'] = pred_y.flat[:]
-    res.to_csv('../data/submission_1.csv',index=False)
+    res.to_csv('../data/answer'+model_name+'.csv',index=False)
     print(pred_y.flat[:])
     #f_w.write(pred_y)
     f_w.close()
@@ -108,17 +112,24 @@ def load_data(path, offline=True):
     return train, valid
 
 def get_featured_data():
-    train_x = pd.read_csv('../data/noEye/train_x.csv')
-    train_y = pd.read_csv('../data/noEye/train_y.csv')
-    test_x = pd.read_csv('../data/noEye/test_x.csv')
+    #old
+    train_x = util.add_zero(pd.read_csv('../data/features/train_x_f.csv',dtype='float'))
+    train_y = util.add_zero(pd.read_csv('../data/features/train_y_f.csv',dtype='float'))
+    test_x = util.add_zero(pd.read_csv('../data/features/test_x_f.csv',dtype='float'))
+
+    #yangdechuan's data
+    # train = pd.read_csv('../data/WechatTrainData_post.csv',dtype='float')
+    # test_x = pd.read_csv('../data/WechatTestData_post.csv',dtype='float').drop(['综合GPA'],axis=1)
+    # train_y = pd.DataFrame({'综合GPA':train['综合GPA']})
+    # train_x = train.drop(['综合GPA'],axis=1)
     return train_x, train_y, test_x
 
 if __name__ == '__main__':
     t_start = clock()
-    offline = False
     train_x,train_y,test_x = get_featured_data()
-    valid_x = train_x[int(0.8*len(train_x)):len(train_x)]
-    valid_y = train_y[int(0.8*len(train_y)):len(train_y)]
+    x_train, x_valid, y_train, y_valid = train_test_split(train_x, train_y, test_size = 0.15)
+    #valid_x = train_x[int(0.8*len(train_x)):len(train_x)]
+    #valid_y = train_y[int(0.8*len(train_y)):len(train_y)]
     #weight = float(len(train_y[train_y==0]))/len(train_y[train_y==1])
     #class_weight = {1:weight,0:1}
 
@@ -127,52 +138,59 @@ if __name__ == '__main__':
     print (train_y.shape)
     print (test_x.shape)
 
-    dtrainV = xgb.DMatrix(train_x[0:int(0.8*len(train_x))],label=train_y[0:int(0.8*len(train_y))])
+    dtrainV = xgb.DMatrix(x_train,label=y_train)
     dtrainA = xgb.DMatrix(train_x,label=train_y)
     dtest = xgb.DMatrix(test_x)
-    dvalid = xgb.DMatrix(valid_x,label=valid_y)
-    del train_x,train_y,test_x
+    dvalid = xgb.DMatrix(x_valid,label=y_valid)
+    del train_x,train_y,test_x,x_train,x_valid,y_train,y_valid
     #gc.collect()
 
+    offline = False
     Auto = True
+    depthLst = [5,6,7,8]
     if Auto:
         if offline:
-            xgb_log = open('./log/xgb_log.txt',mode='w')
-            tune_xgb(dtrainV, dvalid)
-            xgb_log.close()
+            for ix in depthLst:
+                model_name = 'xgb_depth_' + str(ix) + '_yangdechuan'
+                xgb_log = open('./log/xgb_log'+model_name+'.txt',mode='w')
+                tune_xgb(dtrainV, dvalid, model_name, depth = ix)
+                xgb_log.close()
         else:
-            tune_reuslt_file = "./log/tune_" + model_name
-            f_w = open(tune_reuslt_file, 'rb')
-            tune_xgb = pickle.load(f_w)
-            f_w.close()
-            
-            best_n_estimators = int(tune_xgb['n_estimators'])
-            best_learning_rate = tune_xgb['learning_rate']
-            #best_max_depth = int(tune_xgb['max_depth'])
-            best_subsample = tune_xgb['subsample']
-            best_colsample_bytree = tune_xgb['colsample_bytree']
-
-            init_params = {
-                'booster': 'gbtree',
-                'objective': 'reg:linear',
-                #'scale_pos_weight': weight,
-                'max_depth': 6,#best_max_depth,
-                'subsample': best_subsample,
-                'nthread': n_jobs,
-                'eval_metric': 'rmse',
-                'colsample_bytree': best_colsample_bytree,
-                'eta': best_learning_rate
-            }
-            test(dtrainA,dtest,best_n_estimators)
+            for ix in depthLst:
+                model_name = 'xgb_depth_' + str(ix) + '_yangdechuan'
+                tune_reuslt_file = "./log/tune_" + model_name
+                f_w = open(tune_reuslt_file, 'rb')
+                tune_xgb = pickle.load(f_w)
+                f_w.close()
+                
+                best_n_estimators = int(tune_xgb['n_estimators'])
+                best_learning_rate = tune_xgb['learning_rate']
+                #best_max_depth = int(tune_xgb['max_depth'])
+                best_subsample = tune_xgb['subsample']
+                best_colsample_bytree = tune_xgb['colsample_bytree']
+                
+                init_params = {
+                    'booster': 'gbtree', #'dart',
+                    'objective': 'reg:linear',
+                    #'scale_pos_weight': weight,
+                    'max_depth': ix,#best_max_depth,
+                    'subsample': best_subsample,
+                    'nthread': n_jobs,
+                    'eval_metric': 'rmse',
+                    'colsample_bytree': best_colsample_bytree,
+                    'eta': best_learning_rate
+                }
+                test(dtrainA,dtest,best_n_estimators,model_name)
     else:
         init_params = {
-                'booster': 'gbtree',
+                'booster': 'gbtree', #'dart',
                 'objective': 'reg:linear',
-                'max_depth': 8,
+                'max_depth': 6,
                 'eval_metric': 'rmse',
             }
-        test(dtrain,dtest,best_n_estimators=300)
+        test(dtrainA,dtest,best_n_estimators=300,model_name='FalseAutoDART')
         
 
     t_finish = clock()
     print('==============Costs time : %s s==============' % str(t_finish - t_start))
+
